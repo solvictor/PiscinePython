e33f7e1b66ccdf57daec956cd1a016d1c592c802
solvictor <victor.egret.pro@gmail.com>
2024-06-19 15:48:24 +0200

    clean zoom

--- Python01/ex03/zoom.py
+++ Python01/ex03/zoom.py
@@ -6,21 +6,24 @@ import numpy as np
 def main():
     """Load animal.jpgs an zoom it in"""
 
-    pixels = ft_load("animal.jpg")
+    pixels = ft_load("animal.jpeg")
     if pixels is None:
         return
+
+    print(pixels)
 
     width = pixels.shape[1]
     height = pixels.shape[0]
     center_x = width // 2
     center_y = height // 2
-    zoom_factor = 0.6
+    zoom_factor = 0.52
     half_square = round(min(height, width) * 0.5 * zoom_factor)
     zoomed = pixels[
         center_y - half_square: center_y + half_square,
-        center_x - half_square: center_x + half_square,
+        center_x - half_square: center_x + half_square
     ]
-    zoomed = np.dot(zoomed[:, :, :3], [0.299, 0.587, 0.114])
+
+    zoomed = np.dot(zoomed, [0.299, 0.587, 0.114])
 
     print("New shape after slicing:", zoomed.shape)
     print(zoomed)
