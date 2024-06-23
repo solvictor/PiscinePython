0442c11bd6db6f0d75694bbcb49deb0d252ad440
solvictor <victor.egret.pro@gmail.com>
2024-06-23 19:13:51 +0200

    Python03 WIP

---
+++ Python03/ex01/S1E7.py
@@ -0,0 +1,80 @@
+from S1E9 import Character
+
+
+class Baratheon(Character):
+    """Representing the Baratheon family."""
+
+    def __init__(self, first_name: str, is_alive=True) -> None:
+        """Create a Baratheon"""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Baratheon"
+        self.eyes = "brown"
+        self.hairs = "dark"
+
+    def die(self) -> None:
+        """Kills the Baratheon"""
+        self.is_alive = False
+
+    def __str__(self) -> str:
+        """Baratheon string representation"""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self) -> str:
+        """Baratheon debugging string representation"""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+
+class Lannister(Character):
+    """Representing the Lannister family."""
+
+    def __init__(self, first_name: str, is_alive=True) -> None:
+        """Create a Lannister"""
+        super().__init__(first_name, is_alive)
+        self.family_name = "Lannister"
+        self.eyes = "blue"
+        self.hairs = "light"
+
+    def die(self) -> None:
+        """Kills the Lannister"""
+        self.is_alive = False
+
+    def __str__(self) -> str:
+        """Lannister string representation"""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    def __repr__(self) -> str:
+        """Lannister debugging string representation"""
+        return f"Vector: ('{self.family_name}', '{self.eyes}', '{self.hairs}')"
+
+    @classmethod
+    def create_lannister(cls, *args, **kwargs):
+        """Create a Lannister
+
+        Returns:
+            Lannister: created Lannister
+        """
+        return Lannister(*args, **kwargs)
+
+
+def main():
+    Robert = Baratheon("Robert")
+    print(Robert.__dict__)
+    print(Robert.__str__)
+    print(Robert.__repr__)
+    print(Robert.is_alive)
+    Robert.die()
+    print(Robert.is_alive)
+    print(Robert.__doc__)
+    print("---")
+    Cersei = Lannister("Cersei")
+    print(Cersei.__dict__)
+    print(Cersei.__str__)
+    print(Cersei.is_alive)
+    print("---")
+    Jaine = Lannister.create_lannister("Jaine", True)
+    print(f"Name : {Jaine.first_name, type(Jaine).__name__}, \
+Alive : {Jaine.is_alive}")
+
+
+if __name__ == "__main__":
+    main()
---
+++ Python03/ex01/S1E9.py
@@ -0,0 +1,48 @@
+from abc import ABC, abstractmethod
+
+
+class Character(ABC):
+    """Character class"""
+
+    def __init__(self, first_name: str, is_alive=True) -> None:
+        """Create a Character named first_name
+
+        Args:
+            first_name (str): Name of the character
+            is_alive (bool, optional): State of life. Defaults to True.
+        """
+        self.first_name = first_name
+        self.is_alive = is_alive
+
+    @abstractmethod
+    def die(self) -> None:
+        """Kills the Character"""
+        pass
+
+
+class Stark(Character):
+    """Implementation of Character"""
+
+    def die(self) -> None:
+        """Kills the Stark"""
+        self.is_alive = False
+
+
+def main():
+    Ned = Stark("Ned")
+    print(Ned.__dict__)
+    print(Ned.is_alive)
+    Ned.die()
+    print(Ned.is_alive)
+    print(Ned.__doc__)
+    print(Ned.__init__.__doc__)
+    print(Ned.die.__doc__)
+    print("---")
+    Lyanna = Stark("Lyanna", False)
+    print(Lyanna.__dict__)
+
+    # hodor = Character("hodor") # Must produce error
+
+
+if __name__ == "__main__":
+    main()
