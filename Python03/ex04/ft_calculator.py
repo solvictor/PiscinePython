1d3d840661099a4e974212b5ea7ba0f9012897e0
solvictor <victor.egret.pro@gmail.com>
2024-06-24 15:44:25 +0200

    Added docstrings

--- Python03/ex04/ft_calculator.py
+++ Python03/ex04/ft_calculator.py
@@ -2,16 +2,37 @@ class calculator:
 
     @staticmethod
     def dotproduct(V1: list[float], V2: list[float]) -> None:
+        """Compute the dot product of two vectors
+
+        Args:
+            V1 (list[float]): first vector
+            V2 (list[float]): second vector
+        """
+
         dot = sum(a * b for a, b in zip(V1, V2))
         print("Dot product is:", dot)
 
     @staticmethod
     def add_vec(V1: list[float], V2: list[float]) -> None:
+        """Compute the sum of two vectors
+
+        Args:
+            V1 (list[float]): first vector
+            V2 (list[float]): second vector
+        """
+
         add = [float(a + b) for a, b in zip(V1, V2)]
         print("Add Vector is :", add)
 
     @staticmethod
     def sous_vec(V1: list[float], V2: list[float]) -> None:
+        """Compute the substraction of two vectors
+
+        Args:
+            V1 (list[float]): first vector
+            V2 (list[float]): second vector
+        """
+
         sous = [float(a - b) for a, b in zip(V1, V2)]
         print("Sous Vector is:", sous)
 
